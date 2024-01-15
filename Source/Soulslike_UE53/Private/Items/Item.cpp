688043282118317684a121223cd49a633adb2165
Vp_pDoochi <wow2658@naver.com>
2024-01-16 00:14:20 +0900

    [dev]set: Create DebugMacros, labels, Openlevelmap

---
+++ Source/Soulslike_UE53/Private/Items/Item.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Items/Item.h"
+#include "Soulslike_UE53/DebugMacros.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Soulslike_UE53/Public/Items/Item.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class SOULSLIKE_UE53_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};
